4c1c4ad097e4bdff127cd7daf3a0a79c478d2120
kaouther <kaoutherboutheldja@gmail.com>
2023-09-29 21:46:54 +0100

    A grade calculator using if else statement

---
+++ Grade-calculator.py
@@ -0,0 +1,12 @@
+score = float(input("Enter your score: "))
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+print("Your grade is:", grade)
