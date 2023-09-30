5849d8b82b67a511baf82dc8a27af3d2be0b227b
kaouther <kaoutherboutheldja@gmail.com>
2023-09-30 23:18:58 +0100

    add files

---
+++ decimalToBinary.py
@@ -0,0 +1,6 @@
+def decimal_to_binary():
+    decimal = int(input("Enter a decimal number: "))
+    binary = bin(decimal)[2:]
+    print("The binary representation is:", binary)
+
+decimal_to_binary()
