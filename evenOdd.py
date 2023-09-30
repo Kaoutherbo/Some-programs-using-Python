3194b74ea9b16c4a19b818ea073d96cd68717c69
kaouther <kaoutherboutheldja@gmail.com>
2023-09-30 23:04:59 +0100

    add files

---
+++ evenOdd.py
@@ -0,0 +1,8 @@
+def check_even_or_odd():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd")
+
+check_even_or_odd()
