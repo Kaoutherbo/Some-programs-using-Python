120522c2733b34f1639b972ec923e13cf8e8adc2
kaouther <kaoutherboutheldja@gmail.com>
2023-09-30 23:14:12 +0100

    add files

---
+++ findMax.py
@@ -0,0 +1,8 @@
+def find_maximum():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    num3 = float(input("Enter the third number: "))
+    maximum = max(num1, num2, num3)
+    print("The maximum number is:", maximum)
+
+find_maximum()
