15c3075a9915490f6a831e5dd4e5dec5c95e964e
kaouther <kaoutherboutheldja@gmail.com>
2023-10-08 23:24:54 +0100

    add more files

---
+++ Lists/list1.py
@@ -0,0 +1,4 @@
+str = "Leo Messi is the greatest player of all time"
+print(str.split(' ')) # it will split the string into this list ['Leo' ,'Messi', 'is', 'the', 'greatest', 'player', 'of', 'all', 'time']
+
+
