f1fe694b23c3d7251198c8d75df92b96c9360de5
kaouther <kaoutherboutheldja@gmail.com>
2023-11-07 23:00:15 +0100

    Add file

--- problems/fibonace.py
+++ problems/fibonace.py
@@ -1,11 +1,11 @@
 # generate fibonacci function using recursion
-def generate_fibonacci(n):
+def generate_fibonacci(num):
     fib_sequence = [0, 1]
-    while len(fib_sequence) < n:
+    while len(fib_sequence) < num:
         next_num = fib_sequence[-1] + fib_sequence[-2]
         fib_sequence.append(next_num)
     return fib_sequence
 
-n = int(input("Enter the number of Fibonacci terms to generate: "))
-fibonacci_sequence = generate_fibonacci(n)
+num = int(input("Enter the number of Fibonacci terms to generate: "))
+fibonacci_sequence = generate_fibonacci(num)
 print("Fibonacci Sequence:", fibonacci_sequence)
