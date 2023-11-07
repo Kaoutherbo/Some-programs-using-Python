a72884696bdb9f388e2b80337d2eb8c16656fbe5
kaouther <kaoutherboutheldja@gmail.com>
2023-11-07 23:02:40 +0100

    Add file

--- problems/findLcm.py
+++ problems/findLcm.py
@@ -2,12 +2,12 @@ def find_lcm():
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
     
-    def gcd(a, b):
+    def pgcd(a, b):
         if b == 0:
             return a
-        return gcd(b, a % b)
+        return pgcd(b, a % b)
     
-    lcm = (num1 * num2) // gcd(num1, num2)
+    lcm = (num1 * num2) // pgcd(num1, num2)
     print("The least common multiple (LCM) is:", lcm)
 
 find_lcm()
--- problems/findPgcd.py
+++ problems/findPgcd.py
@@ -1,10 +1,10 @@
-def find_gcd():
+def find_pgcd():
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
     
     while num2:
         num1, num2 = num2, num1 % num2
 
-    print("The greatest common divisor (GCD) is:", num1)
+    print("The greatest common divisor (PGCD) is:", num1)
 
-find_gcd()
+find_pgcd()
